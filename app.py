942b15676e421ed91fb7a16473fe2273c5f00864
DABZEN <Dabbeyestach@hotmail.com>
2025-04-28 19:03:45 +0200

    'Startat upp servern för MauPort via Python med Flask.
    -Nya mappar har skapats: templates för alla html filer och static för css och bilder.
    Filerna har organiserats enligt detta.
    -Statiska portfolios har tagits bort för att lämna plats till MUST-krav F-FIL-1 - Ladda upp filer.

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+# Route till startsidan
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route till login sidan
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Route till profil sidan
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+# Route till portfoliosidan
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+# Route till felhantering 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+
+
+
+# Säkerställer att fel meddelas under testning
+if __name__ == '__main__':
+    app.run(debug=True)
