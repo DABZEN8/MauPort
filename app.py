fd70e3d33ad2f379a8504c76c431551a7d1ffd92
GorSla14 <slavnicgoran@hotmail.com>
2025-05-02 11:31:52 +0200

    Logout-funktion + secret key
    Skapade logout-funktion och la till en secret key i app.py.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from auth import auth
 
 app = Flask (__name__)
+app.secret_key = '3d9f728e4c2b3f6f8e5a1c7a8b9d2f3e4c5a6b7d8e9f0a1b2c3d4e5f6a7b8c9d'
 
 app.register_blueprint(auth, url_prefix='/auth')
 
