efe20fb388f191765d98c0e60719d5c063c8312f
schibrach <sandieschibrach@gmail.com>
2025-05-10 15:02:26 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,7 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms import StringField, TextAreaField, FileField
+from wtforms.validators import InputRequired, DataRequired
 
 class UploadFileForm(FlaskForm):
     file = FileField('Välj fil', validators=[InputRequired()])
     submit = SubmitField('Ladda upp')
+
+# Formulär där användare ändrar inställningar i inloggat läge
+class SettingsForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    program = StringField('Program')
+    bio = TextAreaField('Bio')
+    profile_picture = FileField('Profile Picture')
