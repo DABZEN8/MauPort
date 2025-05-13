49c805e9ab60c8deca1424bd75310a305ec9cf49
schibrach <sandieschibrach@gmail.com>
2025-05-13 12:24:04 +0200

    Nu går det att ändra inställningar för aktuell användare

--- forms.py
+++ forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import FileField
-from wtforms import StringField, TextAreaField, FileField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 
 # Formulär där användare ändrar inställningar i inloggat läge
 class SettingsForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    program = StringField('Program')
-    bio = TextAreaField('Bio')
-    profile_picture = FileField('Profile Picture')
+    first_name = StringField("Förnamn", validators=[DataRequired()])
+    last_name = StringField("Efternamn", validators=[DataRequired()])
+    username = StringField ("Användarnamn", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    program = StringField("Program")
+    biography = StringField("Biografi")
+    submit = SubmitField("Spara ändringar")
