6b34a9f5e1cd6949efa6773ee01bb320ce8aae61
Radianceblue <radic.marina91@gmail.com>
2025-06-01 16:04:32 +0200

    Justerat query igen så att de
    nyaste inläggen syns först.

--- app.py
+++ app.py
@@ -40,8 +40,8 @@ def index():
         FROM portfolio p
         JOIN users u ON p.user_id = u.id
         LEFT JOIN portfolio_images img ON p.id = img.portfolio_id
-        ORDER BY p.id, img.img_path ASC
-    """)
+        ORDER BY p.id DESC, img.img_path ASC
+        """)
     portfolios = cur.fetchall()
 
     saved_ids = []
